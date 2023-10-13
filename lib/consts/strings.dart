3fdc6df7f7d5577d5e33a0fc3d061d4c437ac738
sagor <sagorsamadder.official@gmail.com>
2023-10-13 13:03:00 +0600

    fixing bug

--- lib/consts/strings.dart
+++ lib/consts/strings.dart
@@ -1,5 +1,5 @@
 const String degree = "°";
-const String apiKey = "98a5cdd42bd5ba3035448b97be430849";
+const String apiKey = "your api key";
 const String currentEndpoint =
     "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
 const String daysEndpoint =
